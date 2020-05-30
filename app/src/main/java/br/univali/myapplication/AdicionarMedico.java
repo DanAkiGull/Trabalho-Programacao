eea2b16aa2da3185f6cd2163e732daa0c6b1e8ed
Daniel <daniel.gullich@edu.univali.br>
2020-05-30 18:45:18 -0300

    Desenvolvimento 1

--- app/src/main/java/br/univali/myapplication/AdicionarMedico.java
+++ app/src/main/java/br/univali/myapplication/AdicionarMedico.java
@@ -2,13 +2,51 @@ package br.univali.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AdicionarMedico extends AppCompatActivity {
+
+    EditText nome;
+    EditText crm;
+    EditText logradouro;
+    EditText numero;
+    EditText cidade;
+    EditText uf;
+    EditText celular;
+    EditText fixo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_medico);
+
+        nome = findViewById(R.id.edNome);
+        crm = findViewById(R.id.edCrm);
+        logradouro = findViewById(R.id.edLogradouro);
+        numero = findViewById(R.id.edNumero);
+        cidade = findViewById(R.id.edCidade);
+        uf = findViewById(R.id.edUF);
+        celular = findViewById(R.id.edCelular);
+        fixo = findViewById(R.id.edFixo);
     }
+
+    public void adicionarMedico(View v){
+
+        String nomeString = nome.getText().toString();
+        String crmString = crm.getText().toString();
+        String logradouroString = logradouro.getText().toString();
+        String numeroString = numero.getText().toString();
+        String cidadeString = cidade.getText().toString();
+        String ufString = uf.getText().toString();
+        String celularString = celular.getText().toString();
+        String fixoString = fixo.getText().toString();
+
+        Toast.makeText(this, "Adicionado com Sucesso ", Toast.LENGTH_SHORT).show();
+    }
+
 }
--- app/src/main/java/br/univali/myapplication/MainActivity.java
+++ app/src/main/java/br/univali/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package br.univali.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_addMedico = (Button)findViewById(R.id.btn_addMedico);
+        btn_addMedico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Intent i = new Intent(MainActivity.this, AdicionarMedico.class);
+                    startActivity(i);
+            }
+        });
+
+        Button btn_addPaciente = (Button)findViewById(R.id.btn_addPaciente);
+        btn_addPaciente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AdicionarPaciente.class);
+                startActivity(i);
+            }
+        });
+
+        Button btn_addConsulta = (Button)findViewById(R.id.btn_addConsulta);
+        btn_addConsulta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AdicionarConsulta.class);
+                startActivity(i);
+            }
+        });
     }
 }
