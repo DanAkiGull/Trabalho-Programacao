31cb870eaa6fdeedb70240046edf47e9df6c6030
Daniel <daniel.gullich@edu.univali.br>
2020-05-30 23:56:56 -0300

    Edicao criada

---
+++ app/src/main/java/br/univali/myapplication/EditarMedico.java
@@ -0,0 +1,14 @@
+package br.univali.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditarMedico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_medico);
+    }
+}
--- app/src/main/java/br/univali/myapplication/ListarMedico.java
+++ app/src/main/java/br/univali/myapplication/ListarMedico.java
@@ -3,11 +3,16 @@ package br.univali.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ListarMedico extends AppCompatActivity {
@@ -19,8 +24,42 @@ public class ListarMedico extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_medico);
+
         lvMedicos = findViewById(R.id.lvMedicos);
+
         listarMedicos();
+
+        lvMedicos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                View v = lvMedicos.getChildAt(position);
+                TextView tvListIdMedico = v.findViewById(R.id.tvListIdMedico);
+                TextView tvListNomeMedico = v.findViewById(R.id.tvListNomeMedico);
+                TextView tvListCrmMedico = v.findViewById(R.id.tvListCrmMedico);
+                TextView tvListLogradouroMedico = v.findViewById(R.id.tvListLogradouroMedico);
+                TextView tvListNumeroMedico = v.findViewById(R.id.tvListNumeroMedico);
+                TextView tvListCidadeMedico = v.findViewById(R.id.tvListCidadeMedico);
+                TextView tvListUfMedico = v.findViewById(R.id.tvListUfMedico);
+                TextView tvListCelularMedico = v.findViewById(R.id.tvListCelularMedico);
+                TextView tvListFixoMedico = v.findViewById(R.id.tvListFixoMedico);
+
+                Intent i = new Intent(getApplicationContext(), EditarMedico.class);
+
+                i.putExtra("_id",tvListIdMedico.getText().toString());
+                i.putExtra("nome",tvListNomeMedico.getText().toString());
+                i.putExtra("crm",tvListCrmMedico.getText().toString());
+                i.putExtra("logradouro",tvListLogradouroMedico.getText().toString());
+                i.putExtra("numero",tvListNumeroMedico.getText().toString());
+                i.putExtra("cidade",tvListCidadeMedico.getText().toString());
+                i.putExtra("uf",tvListUfMedico.getText().toString());
+                i.putExtra("celular",tvListCelularMedico.getText().toString());
+                i.putExtra("fixo",tvListFixoMedico.getText().toString());
+
+                startActivity(i);
+            }
+        });
+
     }
 
     private void listarMedicos() {
