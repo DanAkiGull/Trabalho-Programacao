6318cf038a204ee2d5926369d1008c391b20c4ea
Daniel <daniel.gullich@edu.univali.br>
2020-05-31 18:40:06 -0300

    Erros corrigidos

---
+++ app/src/main/java/br/univali/myapplication/EditarConsulta.java
@@ -0,0 +1,14 @@
+package br.univali.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditarConsulta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_consulta);
+    }
+}
--- app/src/main/java/br/univali/myapplication/ListarConsulta.java
+++ app/src/main/java/br/univali/myapplication/ListarConsulta.java
@@ -3,11 +3,15 @@ package br.univali.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class ListarConsulta extends AppCompatActivity {
 
@@ -23,6 +27,31 @@ public class ListarConsulta extends AppCompatActivity {
 
         listarConsultas();
 
+        lvConsultas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                View v = lvConsultas.getChildAt(position);
+                TextView tvListIdConsulta = v.findViewById(R.id.tvListIdConsulta);
+                TextView tvListIdPaciente = v.findViewById(R.id.tvListIdPaciente);
+                TextView tvListIdMedico = v.findViewById(R.id.tvListIdMedico);
+                TextView tvListInicioConsulta = v.findViewById(R.id.tvListInicioConsulta);
+                TextView tvListFimConsulta = v.findViewById(R.id.tvListFimConsulta);
+                TextView tvListObservacaoConsulta = v.findViewById(R.id.tvListObservacaoConsulta);
+
+                Intent i = new Intent(getApplicationContext(), EditarPaciente.class);
+
+                i.putExtra("_id",tvListIdConsulta.getText().toString());
+                i.putExtra("paciente_id",tvListIdPaciente.getText().toString());
+                i.putExtra("medico_id",tvListIdMedico.getText().toString());
+                i.putExtra("data_hora_inicio",tvListInicioConsulta.getText().toString());
+                i.putExtra("data_hora_fim",tvListFimConsulta.getText().toString());
+                i.putExtra("observacao",tvListObservacaoConsulta.getText().toString());
+
+                startActivity(i);
+            }
+        });
+
     }
 
     private void listarConsultas() {
@@ -34,7 +63,7 @@ public class ListarConsulta extends AppCompatActivity {
         Cursor dados = db.rawQuery(sql_builder.toString(),null);
 
         String[] from = {"_id", "paciente_id", "medico_id", "data_hora_inicio", "data_hora_fim", "observacao"};
-        int[] to = {R.id.tvListIdConsulta, R.id.tvListIdPaciente, R.id.tvListIdMedico, R.id.tvListInicioConsulta,
+        int[] to = {R.id.tvListIdConsulta, R.id.tvListPacienteConsulta, R.id.tvListMedicoConsulta, R.id.tvListInicioConsulta,
                 R.id.tvListFimConsulta, R.id.tvListObservacaoConsulta};
 
         SimpleCursorAdapter scAdapter = new SimpleCursorAdapter(getApplicationContext(), R.layout.dados_consulta, dados, from, to, 0);
